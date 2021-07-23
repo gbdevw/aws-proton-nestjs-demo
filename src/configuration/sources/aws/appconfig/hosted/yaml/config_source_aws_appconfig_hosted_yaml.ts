6b3ec38d4eb5032144a2f650f0204d44120ca717
gbdevw <guillaume.braibant@outlook.com>
2021-07-23 10:07:11 +0000

    Add AWS App Config. config. source + test

---
+++ src/configuration/sources/aws/appconfig/hosted/yaml/config_source_aws_appconfig_hosted_yaml.ts
@@ -0,0 +1,45 @@
+// Import AWS App Config libs
+import { AppConfigClient, GetConfigurationCommand, GetConfigurationCommandOutput } from '@aws-sdk/client-appconfig';
+
+// Import 3rd party
+import { v4 as uuidv4 } from 'uuid';
+import * as yaml from 'js-yaml';
+
+/**
+ * Loads config. from a YAML hosted configuration on AWS App Config
+ * @param application Name of the application related to the config.
+ * @param env Target environment
+ * @param configuration Name of the confguration profile
+ * @param clientid Optional - Client ID for config. deployment monitoring - Defaults to random uuid
+ * @param version Optional - Target config. version - Defaults to latest version
+ * @returns Loaded config. keys
+ */
+export async function load_yaml_config_from_app_config_hosted(region: string, application: string, env: string, configuration: string, clientid?: string, version?: string): Promise<Record<string, any>> {
+
+    // Create client - config. from env.
+    const client = new AppConfigClient({region: region});
+
+    try {
+        // Fetch config.
+        var response = await client.send(new GetConfigurationCommand(
+            {
+                Application: application,
+                Environment: env,
+                ClientId: clientid || uuidv4(),
+                Configuration: configuration,
+                ClientConfigurationVersion: version
+            }
+        ))
+
+        // Load config as utf-8 encoded yaml doc
+        let data = yaml.load(
+            (new TextDecoder('utf-8')).decode(response.Content)
+        ) as Record<string, any>;
+        console.log(data)
+        return data
+    }
+    catch(error) {
+        // Reraise error
+        throw error
+    }
+};
---
+++ test/integration/configuration/source/aws/appconfig/hosted/yaml/config_source_aws_appconfig_hosted_yaml.spec.ts
@@ -0,0 +1,45 @@
+// Import config. source
+import { load_yaml_config_from_app_config_hosted } from '../../../../../../../../src/configuration/sources/aws/appconfig/hosted/yaml/config_source_aws_appconfig_hosted_yaml'
+
+/**
+ * Test configration from AWS App Config.
+ */
+describe('Test - Configuration Source - Aws App Config', () => {
+
+    /**
+     * Load a regular config. from AWS App Config.
+     */
+    test ('Load regular config. from AWS App Config', async () => {
+
+        // Load config. from yaml
+        let config = await load_yaml_config_from_app_config_hosted(
+            'eu-west-1',
+            'aws-proton-nest-js-demo',
+            'dev',
+            'app-config'
+        )
+
+        // Test application related keys
+        expect(config.application.port).toBe(3000)
+        expect(config.application.hostname).toBe('0.0.0.0')
+        expect(config.application.service).toBe('Coinsight API')
+        expect(config.application.instance).toBe('coinsight-x')
+
+        // Test application related keys
+        // Coinbase
+        expect(config.coinsight.coinbase.enabled).toBe(true)
+        expect(config.coinsight.coinbase.url).toBe('https://api-public.sandbox.pro.coinbase.com')
+        expect(config.coinsight.coinbase.products.length).toBe(2)
+        expect(config.coinsight.coinbase.products.includes('btc-usd')).toBeTruthy
+        expect(config.coinsight.coinbase.products.includes('eth-eur')).toBeTruthy
+        // Kraken
+        expect(config.coinsight.kraken.enabled).toBe(true)
+        expect(config.coinsight.kraken.url).toBe('https://api.kraken.com/0')
+        expect(config.coinsight.kraken.products.length).toBe(1)
+        expect(config.coinsight.kraken.products.includes('xxbtzusd')).toBeTruthy
+
+        
+        // Test logging related keys
+        expect(config.logging.level).toBe('INFO')
+    })
+});
